fc505730eacb94e47e0bd12acd943dfb317cef1d
Arafat Olayiwola <harof.dev@gmail.com>
2021-07-06 01:24:26 +0100

    make request to api

--- app.js
+++ app.js
@@ -4,22 +4,45 @@ const closeBtn = document.getElementById('close');
 
 
 const fetchStudents = async () => {
-  const response = await fetch('127.0.0.1:8000/studentsapi/students')
-  const students = await response.json()
+  const response = await fetch('http://127.0.0.1:8000/api/students/')
+  const students = await response.json();
   return students;
 }
 
+
+let id = 1;
+let output;
+fetchStudents()
+  .then(data => {
+    data.students.forEach(student => {
+      // output = `
+      //   <td>${id++}</td>
+      // `
+      
+      // const TR = document.querySelector('#TR');
+      // TR.append(output)
+    })
+  })
+
+  console.log(output);
+  // <td>student.firstname</td>
+  // <td>student.lastname</td>
+  // <td>student.regno</td>
+  // <td>student.email</td>
+  // <td>student.sex</td>
+  // <td>student.status</td>
+  // <td>
+  //     <button id="full-profile">Profile</button>
+  // </td>
+  // <td>
+  //     <form action="#" method="POST">
+  //         <button type="submit">Delete</button>
+  //     </form>
+  // </td>
 profileBtn.forEach(element => {
   element.addEventListener("click", openDashboard);
-  fetchStudents()
-    .then(data => {
-      console.log(data)
-    })
-    .catch(err => {
-      console.log(err)
-    });
-  
-})
+ 
+});
 closeBtn.addEventListener("click", closeDashboard);
 
 
